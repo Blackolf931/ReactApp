7f31f2abd077d0e1f3ff116d9fb361558babe5b7
DZIANIS KAHUKIN <dzianis.kahukin@innowise-group.com>
2021-12-12 15:12:02 +0300

    Add simple login form

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -75,7 +75,6 @@ export const getUserProfile = (userId) => (dispatch) => {
 export const getStatus = (userId) => (dispatch) => {
     profileAPI.getStatus(userId)
         .then(response => {
-            debugger;
             dispatch(setStatus(response.data));
         });
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -6,6 +6,7 @@ import sidebarReducer from "./sidebar-reducer";
 import userReducer from "./user-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form'
 
 let reducers = combineReducers ({
     profilePage: profileReducer,
@@ -13,10 +14,13 @@ let reducers = combineReducers ({
     officePage: officeReducer,
     sidebar: sidebarReducer,
     usersPage: userReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+window.store = store;
 
 export default store;
