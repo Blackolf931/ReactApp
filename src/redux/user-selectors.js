438dde61b0d39726a981f0dd8cdea051ed0abdbd
Denis Kagykin <denis.kagukin@gmail.com>
2022-01-03 16:31:01 +0300

    Add reselect

--- src/redux/user-selectors.js
+++ src/redux/user-selectors.js
@@ -1,18 +1,26 @@
-export const getUsers =(state) => {
-    return state.usersPage.users;
+import {createSelector} from "reselect";
+
+export const getUsersSelector = (state) => {
+    return getUsers().filter(u=> true);
 }
-export const getPageSize =(state) => {
+
+export const getUsers = createSelector(getUsersSelector,
+    (users) => {
+   return users.usersPage.users.filter(u => true)
+})
+
+export const getPageSize = (state) => {
     return state.usersPage.pageSize;
 }
-export const getTotalUsersCount =(state) => {
+export const getTotalUsersCount = (state) => {
     return state.usersPage.totalUsersCount;
 }
-export const getCurrentPage =(state) => {
+export const getCurrentPage = (state) => {
     return state.usersPage.currentPage;
 }
-export const getIsFetching =(state) => {
+export const getIsFetching = (state) => {
     return state.usersPage.isFetching;
 }
-export const getFollowingInProgress =(state) => {
+export const getFollowingInProgress = (state) => {
     return state.usersPage.followingInProgress;
 }
