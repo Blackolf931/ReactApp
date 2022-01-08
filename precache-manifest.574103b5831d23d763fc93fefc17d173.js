7935e11ce055d7ae58fa641e025be2ab9a56ab7a
Denis Kagykin <denis.kagukin@gmail.com>
2022-01-08 13:31:40 +0300

    Updates

---
+++ precache-manifest.574103b5831d23d763fc93fefc17d173.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d78e54ba1d588a2c21e89790832ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9e5b2fcea4df423a66a",
+    "url": "/static/css/3.903229ce.chunk.css"
+  },
+  {
+    "revision": "915d35d66f62e5edbf9b",
+    "url": "/static/css/4.a5b228e6.chunk.css"
+  },
+  {
+    "revision": "15c812e662166a34249e",
+    "url": "/static/css/main.f826739f.chunk.css"
+  },
+  {
+    "revision": "934811fa2a711ba7513d",
+    "url": "/static/js/2.9c7cc603.chunk.js"
+  },
+  {
+    "revision": "c81fa1e07dc6ecba91f27e5541978909",
+    "url": "/static/js/2.9c7cc603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9e5b2fcea4df423a66a",
+    "url": "/static/js/3.a451b486.chunk.js"
+  },
+  {
+    "revision": "915d35d66f62e5edbf9b",
+    "url": "/static/js/4.d575e780.chunk.js"
+  },
+  {
+    "revision": "b0319a6865587653c023",
+    "url": "/static/js/5.e75b1a9d.chunk.js"
+  },
+  {
+    "revision": "75d1960c00e945e14c66",
+    "url": "/static/js/6.d9d4799e.chunk.js"
+  },
+  {
+    "revision": "15c812e662166a34249e",
+    "url": "/static/js/main.5bab80c6.chunk.js"
+  },
+  {
+    "revision": "4b5a7ebebf4d00bdf07c",
+    "url": "/static/js/runtime-main.8c2683d9.js"
+  },
+  {
+    "revision": "b27593998efd5e827ca75299977d056c",
+    "url": "/static/media/notFoundJob.b2759399.png"
+  },
+  {
+    "revision": "0deacccf0c255976d3f5a6bb07151791",
+    "url": "/static/media/preloader.0deacccf.svg"
+  },
+  {
+    "revision": "f7298ff5bc2f8316b80a84f69c7dcf3f",
+    "url": "/static/media/user.f7298ff5.png"
+  }
+]);
